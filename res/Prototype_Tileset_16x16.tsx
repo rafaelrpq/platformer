d93691b8fe69885970d867c9af96243086769bb2
Rafael Quevedo <rafaelrpq@gmail.com>
2024-12-06 13:58:12 -0300

    ajustes tileset

--- res/Prototype_Tileset_16x16.tsx
+++ res/Prototype_Tileset_16x16.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Prototype_Tileset_16x16" tilewidth="16" tileheight="16" tilecount="176" columns="16">
- <image source="../../../../../../../media/rafaelquevedo/dados/assets/Prototype_Tileset_16x16.png" width="256" height="176"/>
+ <image source="Prototype_Tileset_16x16.png" width="256" height="176"/>
 </tileset>
