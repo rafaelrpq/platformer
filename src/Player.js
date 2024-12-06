18d325ffeef6638f550e8bfcf3e27f1fcaeb5680
Rafael Quevedo <rafaelrpq@gmail.com>
2024-12-06 12:55:27 -0300

    slopes implementados

--- src/Player.js
+++ src/Player.js
@@ -29,15 +29,17 @@ export default class Player extends Sprite {
     collideSlope(block) {
         let m = 1 / parseInt(block.value.substring(0, 2))
         let c = parseInt(block.value[2]) * block.height * m
-
-        if (m > 0) c =  c - block.height
-
+        
         let ratio = this.height / this.width
-
-        console.log (ratio)
-
-        this.pos.y = block.pos.y - (this.height/ratio)*1.5 + c - ((this.pos.x - block.pos.x) * Math.abs(m))
+        
+        if (m < 0) {
+            this.pos.y = block.pos.y - (this.height / ratio) + c - (this.pos.x + this.width - block.pos.x) * Math.abs(m);
+        } else {
+            this.pos.y = block.pos.y - (this.height / ratio) + c - (block.height - (this.pos.x - block.pos.x) * Math.abs(m))
+        }
+        
         this.vel.y = 0
+        
         return;
 
     }
